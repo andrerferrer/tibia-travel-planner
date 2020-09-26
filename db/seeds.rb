a4efbf4f10597f996f7de4c400fdef0271bcae49
Andre Ferrer <andrerferrer@hotmail.com>
2020-09-26 15:13:14 -0300

    Finish the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,34 @@
-puts "Clean the DB"
+puts "Cleaning the DB"
 
 if Rails.env == "development"
   City.destroy_all
+  Transportation.destroy_all
 end
 
-puts "Fetch cities"
+puts "Fetching cities"
 
 cities = WebscraperService::Cities.fetch
 
-puts "Create cities"
+puts "Creating cities"
 
-cities.each do |city|
-  City.create! name: city
+cities.each do |city_name|
+  city = City.find_or_create_by! name: city_name
+  puts "Fetching transportations for #{city_name}"
+  transportations = WebscraperService::Transportations.fetch(city_name)
+
+  transportations.each do |transportation|
+    means = transportation[:means]
+    transportation[:destinations].each do |destination|
+      puts "Creating #{transportation[:means]} transportations from #{city_name} to #{destination[:destination_city]}"
+      
+      destination_city = City.find_or_create_by! name: destination[:destination_city]
+
+      Transportation.create!(from_city: city, to_city: destination_city, means: means)
+      
+    end
+  end
 end
+
+
 
 puts "Done!"
