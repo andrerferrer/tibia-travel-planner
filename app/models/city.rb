1ce7fda1d826ec42d49dc693295cc79c8ef981cf
Andre Ferrer <andrerferrer@hotmail.com>
2020-09-26 15:12:54 -0300

    add validation for cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
   has_many :transportations, foreign_key: :from_city_id
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
 end
