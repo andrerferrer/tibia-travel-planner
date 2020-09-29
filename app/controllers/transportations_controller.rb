ff343055e1d87af5f4fdbb52b3c6a660a122ff0b
Andre Ferrer <andrerferrer@hotmail.com>
2020-09-29 17:59:45 -0300

    order cities by name

--- app/controllers/transportations_controller.rb
+++ app/controllers/transportations_controller.rb
@@ -1,6 +1,6 @@
 class TransportationsController < ApplicationController
   def index
-    @cities = City.all
+    @cities = City.order name: :asc
     if params[:search].present?
       origin_city = City.find(params[:search][:origin])
       destination_city = City.find(params[:search][:destination])
