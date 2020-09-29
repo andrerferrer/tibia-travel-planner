8dc5fa9b162683b2cd9610ed40236027c047ffe9
Andre Ferrer <andrerferrer@hotmail.com>
2020-09-29 13:51:15 -0300

    add a new means to transportation

--- app/models/transportation.rb
+++ app/models/transportation.rb
@@ -2,5 +2,5 @@ class Transportation < ApplicationRecord
   belongs_to :from_city, class_name: 'City'
   belongs_to :to_city, class_name: 'City'
 
-  validates :means, presence: true, inclusion: { in: ['boat', 'magic carpet', 'steamship'] }
+  validates :means, presence: true, inclusion: { in: ['boat', 'magic carpet', 'steamship', 'quest'] }
 end
